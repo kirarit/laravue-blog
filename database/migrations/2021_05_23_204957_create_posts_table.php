5f0f5c6461deb67eb0e7d574a915c876809ca506
David Kirarit <davidkrarit@gmail.com>
2021-05-26 23:25:07 +0300

    added soft delete

--- database/migrations/2021_05_23_204957_create_posts_table.php
+++ database/migrations/2021_05_23_204957_create_posts_table.php
@@ -17,6 +17,7 @@ class CreatePostsTable extends Migration
             $table->bigIncrements('id');
             $table->string('title');
             $table->text('description');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
